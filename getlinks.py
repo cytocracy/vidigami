6966ed9eef04b543fee16910fd5d69ab840293a4
cytocracy <theop.parker@gmail.com>
2023-11-13 22:21:51 -0800

    rate limiting

--- getlinks.py
+++ getlinks.py
@@ -5,7 +5,7 @@ def getlinks():
     links   = []
     f = open('nums.txt', 'r')
     for line in f:
-        link = "https://media.vidigami.com/media/production_images_" + line.strip() + "/dyn/max_D/2560-2560"
+        link = "https://media.vidigami.com/media/production_images_" + line.strip() + "/dyn/max_D/4672-4672"
         # print(link)
         links.append(link)
 
--- image.py
+++ image.py
@@ -4,22 +4,33 @@ import shutil
 import getlinks
 import time
 
-link = "https://media.vidigami.com/media/production_images_32699338/dyn/max_D/2560-2560"
-cookies = {'production-global-vidigami': 'edb6c25d77a33325a48bdcd5dee8c7cbd74e53f51ff6c2d3a2b5c599e4b653d5'}
+link = "https://media.vidigami.com/media/production_images_32699338/dyn/max_D/4672-4672"
+cookies = {'production-global-vidigami': 'fa135f43eb7550938a60754957c88c1d9c0231fff97315d3e2cb8223726fef09'}
 
 links = getlinks.getlinks()
+links.reverse()
 
 # img_data = requests.get(link, cookies=cookies).content
 # with open("test", 'wb') as handler:
 #     handler.write(img_data)
 
 cnt = 1
-for url in links:
+
+while len(links) > 0:
+
+    url = links.pop()
     print(url)
 
-    img_data = requests.get(url, cookies=cookies).content
-    with open(str(cnt) + ".png", 'wb') as handler:
-        handler.write(img_data)
+    try:
 
-    cnt += 1
-    time.sleep(0.1)
+        img_data = requests.get(url, cookies=cookies, timeout=5).content
+        print('get done')
+        with open(str(cnt) + ".png", 'wb') as handler:
+            handler.write(img_data)
+        cnt += 1
+    except:
+        print("timeout")
+        links.append(url)
+
+        
+    time.sleep(1)
